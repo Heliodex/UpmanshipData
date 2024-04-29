8e47f05bfb4a423fc2f438a2087288d7d0fa6f4e
Lewin Kelly <heli@odex.cf>
2024-04-29 11:27:22 +0100

    Initialise stats site with SvelteKit

---
+++ site/svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from "@sveltejs/adapter-static"
+import preprocess from "svelte-preprocess"
+
+export default {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+	},
+
+	vitePlugin: {
+		inspector: {
+			toggleKeyCombo: "control-i",
+		},
+	},
+}
